a5d4a97d7c2ecf3e6b8d3c7e992ca89b355aecbd
FizariusNetuno <112980188+PauloDev07@users.noreply.github.com>
2022-11-13 21:56:44 -0300

    Integração das duas partes

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ function resetar() {
     })
 
     for (produto in transacao) {
-     //   let select = document.getElementById('buysell');
-     //   let tipo = select.options[select.selectedIndex].value;
+      //  let select = document.getElementById('tipo');
+       // let tipo = select.options[select.selectedIndex].value;
         document.querySelector('.tabela').innerHTML += `
         <tr class="dados">
             <td style="border-bottom: solid 1px #979797; padding:8px">${transacao[produto].tipo}</td>
             <td style="border-bottom: solid 1px #979797; padding:8px">${transacao[produto].nome}</td>
             <td style="border-bottom: solid 1px #979797; padding:8px; text-align: right;">${transacao[produto].valor}</td>
-            <td> <button onclick="alteraritem(${produto})"> Alterar </button></td>
+            <td> <button onclick="removeritem(${produto})"> Remover </button></td>
         </tr>`
 };}
 
-function alteraritem(alt) {
-    transacao.splice(alt, 1);
+function removeritem(rem) {
+    transacao.splice(rem, 1);
     resetar();
     localStorage.setItem('transacao', JSON.stringify(transacao))
 } resetar()
@@ -37,3 +37,21 @@ function alteraritem(alt) {
 // INTEGRAÇÃO DE DADOS DO FORMULÁRIO AO EXTRATO
 
 
+function adiciona(add) {
+    add.preventDefault();
+    var infostorage = localStorage.getItem('transacao')
+        if (infostorage != null) {
+          var transacao = JSON.parse(infostorage)
+        } else {
+         var transacao = [];
+    }
+
+    transacao.push({
+        tipo: add.target.elements['tipo'].value,
+        nome: add.target.elements['nome'].value,
+        valor: add.target.elements['valor'].value
+        
+    })
+    localStorage.setItem('transacao', JSON.stringify(transacao))
+    document.getElementById('cadastro').click()
+}
