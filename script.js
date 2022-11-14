00aa1bfeee612fdc581153a88dbc2cdd4cb56904
FizariusNetuno <112980188+PauloDev07@users.noreply.github.com>
2022-11-14 17:23:34 -0300

    Modificações no codigo e adição do "Limpar dados"

--- script.js
+++ script.js
@@ -8,34 +8,15 @@ if (infostorage != null) {
     var transacao = [];
 }
 
-    //Atualização de dados da tabela 
-function resetar() {
-    resetabela = [...document.querySelectorAll('.tabela .dados')];
-    resetabela.forEach((element) => {
-        element.remove() 
-    })
-    
-    //Coleta de dados e inserção no extrato
-    for (produto in transacao) {
-        document.querySelector('.tabela').innerHTML += `
-        <tr class="dados" style="width: 100%; font-size: 14px;">
-        <td style="border-bottom: solid 1px #979797; padding:8px">${transacao[produto].tipo}</td>
+//Coleta de dados e inserção no extrato
+for (produto in transacao) {
+    document.querySelector('.tabela').innerHTML += `
+    <tr class="dados" style="width: 100%; font-size: 14px;">
+        <td style="padding:8px">${transacao[produto].tipo}</td>
         <td style="border-bottom: solid 1px #979797; padding:8px">${transacao[produto].nome}</td>
         <td style="border-bottom: solid 1px #979797; padding:8px; text-align: right;">${transacao[produto].valor}</td>
-        <td> <button onclick="removeritem(${produto})"> Remover </button></td>
-        </tr>`
-};}
-
-    //Remoção de itens
-function removeritem(rem) {
-    transacao.splice(rem, 1);
-    resetar();
-    localStorage.setItem('transacao', JSON.stringify(transacao))
-} resetar()
-
-
-
-
+    </tr>`
+};
 
 // INTEGRAÇÃO DE DADOS DO FORMULÁRIO AO EXTRATO
 
@@ -43,8 +24,8 @@ function removeritem(rem) {
 function adiciona(add) {
     add.preventDefault();    
     //Validação de caracteres numericos em "Valor'"
-    for (n in add.target.elements['valor'].value) {
-        if ('0123456789'.indexOf(add.target.elements['valor'].value[n]) == -1){
+    for (i in add.target.elements['valor'].value) {
+        if ('0123456789'.indexOf(add.target.elements['valor'].value[i]) == -1){
             alert('Apenas números são permitidos no campo Valor!')
             return false
         }}       
@@ -69,13 +50,33 @@ function adiciona(add) {
 
 
 // Máscaras do formulário
-function apenasnumeros(e) {
+function apenasnumeros(num) {
     e.preventDefault()
-    console.log(e)
-        if ((/[0-9]/g).test(e.key)) {
-            e.target.value += e.key
-}}
-
-
+    console.log(num)
+    if ((/[0-9]/g).test(num.key)) {
+        num.target.value += num.key
+    }}
+    
+    function limpardados(){
+        let msg = "Deseja realmente limpar os dados? \nEssa ação removerá todos os dados salvos.";
+        if (confirm(msg) == true) {
+            localStorage.clear();            
+        }         
+    }
+    
 
 //toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+    
+    //Atualização de dados da tabela 
+    //function resetar() {
+        //    resetabela = [...document.querySelectorAll('.tabela .dados')];
+        //    resetabela.forEach((element) => {
+        //        element.remove() 
+        //    })
+        //resetar()
+        //Remoção de itens
+        // <td> <button onclick="removeritem(${produto})"> Remover </button></td>
+        //function removeritem(rem) {
+        //transacao.splice(rem, 1);
+        //resetar();
+        //localStorage.setItem('transacao', JSON.stringify(transacao))}
