3311549911ef1a68a2818462c1e789071b261781
FizariusNetuno <112980188+PauloDev07@users.noreply.github.com>
2022-11-24 19:00:56 -0300

    Responsividade e ajuste no resumo

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
 
 // LISTAGEM DE DADOS, LOCAL STORAGE E INTEGRAÇÃO DE DADOS POR JAVASCRIPT
 
-var infostorage = localStorage.getItem('transacao')
-if (infostorage != null) {
-    var transacao = JSON.parse(infostorage)
-} else {
-    var transacao = [];
-}
+    var infostorage = localStorage.getItem('transacao')
+    if (infostorage != null) {
+        var transacao = JSON.parse(infostorage)
+    } else {
+        var transacao = [];
+    }
 
+    //Coleta de dados e inserção no extrato
+    for  (produto in transacao) {
+        let money = transacao[produto].valor
+        document.querySelector('.tabela tbody').innerHTML += `
+        <tr class="dados" style="width: 100%; font-size: 14px; padding:8px; margin-bottom: 2px;">
+            <td style="border-bottom: solid 1px #979797; text-Align: center">${transacao[produto].tipo}</td>
+            <td style="border-bottom: solid 1px #979797; padding:12px;">${transacao[produto].nome}</td>
+            <td style="border-bottom: solid 1px #979797; text-align: right;"> ${parseFloat(money).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
+        </tr>`
+    };
 
-//Coleta de dados e inserção no extrato
-for  (produto in transacao) {
-    let money = transacao[produto].valor
-    document.querySelector('.tabela tbody').innerHTML += `
-    <tr class="dados" style="width: 100%; font-size: 14px; padding:8px; margin-bottom: 2px;">
-        <td style="border-bottom: solid 1px #979797; text-Align: center">${transacao[produto].tipo}</td>
-        <td style="border-bottom: solid 1px #979797; padding:12px;">${transacao[produto].nome}</td>
-        <td style="border-bottom: solid 1px #979797; text-align: right;"> ${parseFloat(money).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
-    </tr>`
-};
-
-if (transacao.length == 0) {
-    document.querySelector('.tabela').innerHTML = `
-    <tr class="dados">
-        <td></td>
-        <td style="text-Align: center; font-size: 30px; padding-top:100px;"> Nenhuma transação cadastrada</td>
-        <td></td>
-    </tr>`
-};
+    if (transacao.length == 0) {
+        document.querySelector('.tabela').innerHTML = `
+        <tr class="dados">
+            <td></td>
+            <td style="text-Align: center; font-size: 30px; padding-top:100px;"> Nenhuma transação cadastrada</td>
+            <td></td>
+        </tr>`
+    };
 
 // INTEGRAÇÃO DE DADOS DO FORMULÁRIO AO EXTRATO
-
-
-//Integração de localStorage e informações do formulário
-function adiciona(add) {
-    add.preventDefault();    
-    var infostorage = localStorage.getItem('transacao')
-        if (infostorage != null) {
-            var transacao = JSON.parse(infostorage)
-        } else {
-        var transacao = [];
-        }
-    
+    //Integração de localStorage e informações do formulário
+    function adiciona(add) {
+        add.preventDefault();    
+        var infostorage = localStorage.getItem('transacao')
+            if (infostorage != null) {
+                var transacao = JSON.parse(infostorage)
+            } else {
+            var transacao = [];
+            }
+        
     transacao.push({
         tipo: add.target.elements['tipo'].value,
         nome: add.target.elements['nome'].value,
@@ -72,7 +69,7 @@ function adiciona(add) {
     var resumo = ''
     if (total >= 1) {
         resumo = `[LUCRO]`
-    } else {
+    } else if (total <= -1) {
         resumo = `[PREJUÍZO]`
     }   document.querySelector('.resumo').innerHTML = `${resumo}`
 
@@ -80,14 +77,14 @@ function adiciona(add) {
 
 
 // MÁSCARAS DO PROJETO
-//Limpeza de dados 
-function apenasnumeros(num) {
-    e.preventDefault()
-    console.log(num)
-    if ((/[0-9]/g).test(num.key)) {
-        num.target.value += num.key
-    }}
-    
+    //Limpeza de dados 
+    function apenasnumeros(num) {
+        e.preventDefault()
+        console.log(num)
+        if ((/[0-9]/g).test(num.key)) {
+            num.target.value += num.key
+        }}
+        
     //Mensagem de confirmação
     function limpardados(){
         let msg = "Deseja realmente limpar os dados? \nEssa ação removerá todos os dados salvos.";
