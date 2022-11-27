12f198c3cf33b4b2b004d899798e5255202b1c6e
FizariusNetuno <112980188+PauloDev07@users.noreply.github.com>
2022-11-27 11:48:27 -0300

    Ajustes em mascara e adição de desenhatotal

--- script.js
+++ script.js
@@ -6,19 +6,47 @@
         var transacao = JSON.parse(infostorage)
     } else {
         var transacao = [];
-    }
+    };
 
-    //Coleta de dados e inserção no extrato
+function desenhatabela(){
+    document.querySelector('.tabela tbody').innerHTML = '';
+    var total = 0
+
+//Calculo total das mercadorias
     for  (produto in transacao) {
-        let money = transacao[produto].valor
+        if (transacao[produto].tipo == '+'){
+            console.log('+')
+            total += parseFloat((transacao[produto].valor))
+        } else {
+            console.log('-')
+            total -= parseFloat((transacao[produto].valor))
+        };
+        
+        
+//Coleta de dados e inserção no extrato
+    let money = transacao[produto].valor
         document.querySelector('.tabela tbody').innerHTML += `
         <tr class="dados" style="width: 100%; font-size: 14px; padding:8px; margin-bottom: 2px;">
             <td style="border-bottom: solid 1px #979797; text-Align: center">${transacao[produto].tipo}</td>
             <td style="border-bottom: solid 1px #979797; padding:12px;">${transacao[produto].nome}</td>
             <td style="border-bottom: solid 1px #979797; text-align: right;"> ${parseFloat(money).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
-        </tr>`
-    };
+        </tr>`;
 
+
+//Chamada do total e resumo da transação
+        document.querySelector('.total').innerHTML = `${parseFloat(total).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`;
+    
+    var resumo = '';
+        if (total > 0) {
+            resumo = `[LUCRO]`
+        } else if (total < 0) {
+            resumo = `[PREJUÍZO]`
+        }
+        
+        document.querySelector('.resumo').innerHTML = `${resumo}`
+    };   
+  
+//Mensagem para falta de transação cadastrada
     if (transacao.length == 0) {
         document.querySelector('.tabela').innerHTML = `
         <tr class="dados">
@@ -28,73 +56,65 @@
         </tr>`
     };
 
-// INTEGRAÇÃO DE DADOS DO FORMULÁRIO AO EXTRATO
-    //Integração de localStorage e informações do formulário
-    function adiciona(add) {
-        add.preventDefault();    
-        var infostorage = localStorage.getItem('transacao')
-            if (infostorage != null) {
-                var transacao = JSON.parse(infostorage)
-            } else {
-            var transacao = [];
-            }
-        
+};
+
+
+function desenhatotal() {
+    document.querySelector('.tabela tfoot').innerHTML = `
+    <tr class="duolinha">
+        <td ></td>
+        <td>Total</td>
+        <td class="total"></td>
+    </tr>
+        <tr>
+        <td></td>
+        <td></td>
+        <td class="resumo"></td>
+    </tr>`
+};
+
+desenhatotal()
+desenhatabela();
+
+//Integração de localStorage e informações do formulário
+function adiciona(add) {
+    add.preventDefault();
     transacao.push({
         tipo: add.target.elements['tipo'].value,
         nome: add.target.elements['nome'].value,
         valor: add.target.elements['valor'].value
         .replaceAll(".", "")
-        .replaceAll(",", "."),
-    })
-    
+        .replaceAll(",", "."),    
+})
     localStorage.setItem('transacao', JSON.stringify(transacao))
-    document.getElementById('cadastro').click()
-}
-
-    //Calculo total das mercadorias
-    var total = 0
-    for (negocio in transacao) {
-        if (transacao[negocio].tipo == '+'){
-            console.log('+')
-            total += parseFloat((transacao[negocio].valor))
-        } else {
-            console.log('-')
-            total -= parseFloat((transacao[negocio].valor))
-        }
-    }
-    
-    document.querySelector('.total').innerHTML = `${parseFloat(total).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`
-
-    //Resumo da transação
-    var resumo = ''
-    if (total >= 1) {
-        resumo = `[LUCRO]`
-    } else if (total <= -1) {
-        resumo = `[PREJUÍZO]`
-    }   document.querySelector('.resumo').innerHTML = `${resumo}`
-
+    desenhatotal()
+    desenhatabela();
+};
 
 
 
 // MÁSCARAS DO PROJETO
-    //Limpeza de dados 
-    function apenasnumeros(num) {
-        e.preventDefault()
-        console.log(num)
-        if ((/[0-9]/g).test(num.key)) {
-            num.target.value += num.key
-        }}
+//Limpeza de dados 
+function apenasnumeros(num) {
+    e.preventDefault()
+    console.log(num)
+    if ((/[0-9]/g).test(num.key)) {
+        num.target.value += num.key
+    }};
         
-    //Mensagem de confirmação
-    function limpardados(){
-        let msg = "Deseja realmente limpar os dados? \nEssa ação removerá todos os dados salvos.";
-        if (confirm(msg) == true) {
-            localStorage.clear();            
-        }         
-    }
+//Mensagem de confirmação
+function limpardados(){
+    let msg = "Deseja realmente limpar os dados? \nEssa ação removerá todos os dados salvos.";
+    if (confirm(msg) == true) {
+        transacao = []
+        localStorage.clear() 
+        desenhatotal()   
+        desenhatabela();        
+    }         
+};
 
-    //Bloqueio de "cola" no campo valor
-    const cola = document.querySelector('#valor');
-    cola.addEventListener("paste", function(c) {
-        c.preventDefault();
-    });
+//Bloqueio de "cola" no campo valor
+const cola = document.querySelector('#valor');
+cola.addEventListener("paste", function(c) {
+    c.preventDefault();
+});
